af1ed345ccc4ce6872c61bab276a4cfe4c809c15
Unal Yigit Ozulku <uyozulku@gmail.com>
2022-10-30 14:02:34 -0400

    add resources

--- storeshare_api/config/routes.rb
+++ storeshare_api/config/routes.rb
@@ -6,11 +6,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :subletters
+  resources :renters
+  resources :listings
+
   root 'listings#index'
 
-  get 'subletters/:id/all_listings', to: 'listings#index', as: 'all_listings'
-  get 'subletters/:id/my_listings', to: 'listings#my_listings', as: 'my_listings'
+  get 'subletters/:id/all_listings', to: 'listings#index'
+  get 'subletters/:id/my_listings', to: 'listings#my_listings'
   get 'subletters/:user_id/my_listings/:id', to: 'listings#show'
 
-  get 'renters/:id/all_listings', to: 'listings#index', as: 'all_listings'
+  get 'renters/:id/all_listings', to: 'listings#index'
 end
