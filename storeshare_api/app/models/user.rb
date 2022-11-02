b4128b2c000248ac0dd5b96faba994f0894e5724
Alex Rupp-Coppi <amr2311@columbia.edu>
2022-11-02 19:38:54 -0400

    porting auth over to JWT

--- storeshare_api/app/models/user.rb
+++ storeshare_api/app/models/user.rb
@@ -1,12 +1,23 @@
 # frozen_string_literal: true
 
+
+
 class User < ApplicationRecord
+
+  # include Devise::JWT::RevocationStrategies::Denylist
+
   # self.abstract_class = true
   has_many :listings, dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :validatable, # :rememberable,
+         :jwt_authenticatable, jwt_revocation_strategy: JwtDenylist
   # :confirmable, :lockable, :trackable
+
+  # def on_jwt_dispatch(token, payload)
+  #   super
+  #   # do_something(token, payload)
+  # end
 end
