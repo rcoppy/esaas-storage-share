bc4420b38d04fcb085488fc609349178bb9d1a5b
Unal Yigit Ozulku <55817387+uyozulku@users.noreply.github.com>
2022-11-16 19:51:23 -0500

    change renter/subletter from has_many to has_one (#16)

--- storeshare_api/db/schema.rb
+++ storeshare_api/db/schema.rb
@@ -10,38 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
+=======
+ActiveRecord::Schema[7.0].define(version: 20_221_115_024_236) do
+>>>>>>> eaa3337 (change renter/subletter from has_many to has_one (#16))
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.bigint 'record_id', null: false
+    t.bigint 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
+                                                    unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.string 'service_name', null: false
+    t.bigint 'byte_size', null: false
+    t.string 'checksum'
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  create_table 'active_storage_variant_records', force: :cascade do |t|
+    t.bigint 'blob_id', null: false
+    t.string 'variation_digest', null: false
+    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
   end
 
+<<<<<<< HEAD
   create_table "contracts", force: :cascade do |t|
     t.bigint "renter_id"
     t.bigint "subletter_id"
@@ -95,19 +101,37 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["subletter_id"], name: "index_listings_on_subletter_id"
+=======
+  create_table 'conversations', force: :cascade do |t|
+    t.integer 'renter_id'
+    t.integer 'subletter_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.text "body"
-    t.bigint "conversation_id"
-    t.bigint "user_id"
-    t.boolean "read", default: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
-    t.index ["user_id"], name: "index_messages_on_user_id"
+  create_table 'jwt_denylists', force: :cascade do |t|
+    t.string 'jti', null: false
+    t.datetime 'exp', null: false
+    t.index ['jti'], name: 'index_jwt_denylists_on_jti'
+>>>>>>> eaa3337 (change renter/subletter from has_many to has_one (#16))
   end
 
+  create_table 'listings', force: :cascade do |t|
+    t.bigint 'subletter_id'
+    t.string 'title'
+    t.string 'description'
+    t.decimal 'price'
+    t.string 'address'
+    t.string 'city'
+    t.string 'state'
+    t.string 'zip_code'
+    t.string 'square_feet'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['subletter_id'], name: 'index_listings_on_subletter_id'
+  end
+
+<<<<<<< HEAD
   create_table "payments", force: :cascade do |t|
     t.bigint "contract_id"
     t.bigint "subletter_id"
@@ -128,30 +152,34 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_renters_on_user_id"
+=======
+  create_table 'messages', force: :cascade do |t|
+    t.text 'body'
+    t.bigint 'conversation_id'
+    t.bigint 'user_id'
+    t.boolean 'read', default: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['conversation_id'], name: 'index_messages_on_conversation_id'
+    t.index ['user_id'], name: 'index_messages_on_user_id'
+>>>>>>> eaa3337 (change renter/subletter from has_many to has_one (#16))
   end
 
-  create_table "subletters", force: :cascade do |t|
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_subletters_on_user_id"
+  create_table 'renters', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_renters_on_user_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'subletters', force: :cascade do |t|
+    t.bigint 'user_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['user_id'], name: 'index_subletters_on_user_id'
   end
 
+<<<<<<< HEAD
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "contracts", "listings"
@@ -166,4 +194,26 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
   add_foreign_key "payments", "subletters"
   add_foreign_key "renters", "users"
   add_foreign_key "subletters", "users"
+=======
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'address'
+    t.string 'phone_number'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  end
+
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'listings', 'subletters'
+  add_foreign_key 'renters', 'users'
+  add_foreign_key 'subletters', 'users'
+>>>>>>> eaa3337 (change renter/subletter from has_many to has_one (#16))
 end
