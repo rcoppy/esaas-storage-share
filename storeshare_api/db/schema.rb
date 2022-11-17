066ba8f94367960aefe9418519ce425d12d43149
Unal Yigit Ozulku <uyozulku@gmail.com>
2022-11-16 23:09:07 -0500

    resolve merge confs

--- storeshare_api/db/schema.rb
+++ storeshare_api/db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
+
+ActiveRecord::Schema[7.0].define(version: 20_221_115_024_236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,17 +44,23 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'conversations', force: :cascade do |t|
-    t.integer 'renter_id'
-    t.integer 'subletter_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
   create_table 'jwt_denylists', force: :cascade do |t|
     t.string 'jti', null: false
     t.datetime 'exp', null: false
     t.index ['jti'], name: 'index_jwt_denylists_on_jti'
+
+  create_table "contracts", force: :cascade do |t|
+    t.bigint "renter_id"
+    t.bigint "subletter_id"
+    t.bigint "listing_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_contracts_on_listing_id"
+    t.index ["renter_id"], name: "index_contracts_on_renter_id"
+    t.index ["subletter_id"], name: "index_contracts_on_subletter_id"
   end
 
   create_table "listings", force: :cascade do |t|
@@ -80,13 +87,48 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
     t.datetime "updated_at", null: false
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  
+    create_table "conversations", force: :cascade do |t|
+    t.integer "renter_id"
+    t.integer "subletter_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "inquiry_id", null: false
+    t.index ["inquiry_id"], name: "index_conversations_on_inquiry_id"
   end
+
+  
+  create_table "inquiries", force: :cascade do |t|
+    t.bigint "renter_id"
+    t.bigint "listing_id"
+    t.date "start_date"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["listing_id"], name: "index_inquiries_on_listing_id"
+    t.index ["renter_id"], name: "index_inquiries_on_renter_id"
+  end
+
 
   create_table 'renters', force: :cascade do |t|
     t.bigint 'user_id'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.index ['user_id'], name: 'index_renters_on_user_id'
+
+  create_table "payments", force: :cascade do |t|
+    t.bigint "contract_id"
+    t.bigint "subletter_id"
+    t.bigint "renter_id"
+    t.decimal "amount"
+    t.string "payment_type"
+    t.string "payment_status", default: "pending"
+    t.date "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["contract_id"], name: "index_payments_on_contract_id"
+    t.index ["renter_id"], name: "index_payments_on_renter_id"
+    t.index ["subletter_id"], name: "index_payments_on_subletter_id"
   end
 
   create_table "subletters", force: :cascade do |t|
@@ -111,9 +153,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_032339) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'listings', 'subletters'
-  add_foreign_key 'renters', 'users'
-  add_foreign_key 'subletters', 'users'
+
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "contracts", "listings"
+  add_foreign_key "contracts", "renters"
+  add_foreign_key "contracts", "subletters"
+  add_foreign_key "conversations", "inquiries"
+  add_foreign_key "inquiries", "listings"
+  add_foreign_key "inquiries", "renters"
+  add_foreign_key "listings", "subletters"
+  add_foreign_key "payments", "contracts"
+  add_foreign_key "payments", "renters"
+  add_foreign_key "payments", "subletters"
+  add_foreign_key "renters", "users"
+  add_foreign_key "subletters", "users"
 end
