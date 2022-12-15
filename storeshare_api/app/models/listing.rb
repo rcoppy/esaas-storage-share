f72cfb49c2418b813cd7207ecdd3a77d817ba060
Unal Yigit Ozulku <uyozulku@gmail.com>
2022-12-15 16:23:05 -0500

    add filter for listing search

--- storeshare_api/app/models/listing.rb
+++ storeshare_api/app/models/listing.rb
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
 class Listing < ApplicationRecord
+  include Filterable
+
   belongs_to :subletter
   has_one_attached :image, dependent: :destroy
+
+  scope :filter_by_city, ->(city) { where city: city }
+  scope :filter_by_state, ->(state) { where state: }
+  scope :filter_by_zip_code, ->(zip_code) { where zip_code: zip_code }
+
+  # filter by price where the listing's price <= target_price
+  scope :filter_by_price, ->(price) { where('price <= ?', price) }
+
+  # filter by square feet where square_feet >= target_square_feet
+  scope :filter_by_square_feet, ->(square_feet) { where('square_feet >= ?', square_feet) }
 end
