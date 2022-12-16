5c551929a074ba68b276812fd08e988b64efdfa8
Unal Yigit Ozulku <55817387+uyozulku@users.noreply.github.com>
2022-12-16 18:07:17 -0500

    Add `renter_subletter_cannot_be_same` validation to the `Contract` model (#29)
    * add validation
    
    * change err message
    
    * add spec

--- storeshare_api/app/models/contract.rb
+++ storeshare_api/app/models/contract.rb
@@ -10,6 +10,7 @@ class Contract < ApplicationRecord
   validates :start_date, :renter_id, :subletter_id, :listing_id, :price, presence: true
   validate :start_date_cannot_be_in_the_past
   validates :end_date, date: { after_or_equal_to: :start_date }, presence: true
+  validate :renter_subletter_cannot_be_same, on: :create
 
   private
 
@@ -17,5 +18,13 @@ class Contract < ApplicationRecord
     return unless start_date.present? && start_date < Date.today
 
     errors.add(:start_date, 'cannot be in the past')
+  end
+
+  def renter_subletter_cannot_be_same
+    # compare user_id of the renter and subletter
+    return unless renter_id.present? && subletter_id.present?
+    return unless Renter.find(renter_id).user_id == Subletter.find(subletter_id).user_id
+
+    errors.add(:renter_id, 'cannot belong to the same user as the subletter')
   end
 end
